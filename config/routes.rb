e5e34889e9016415b6534f28a2c4a491d9ee07dc
Mostafa abdelsalam <mostafa.abdelsalam96@gmail.com>
2021-08-10 18:46:52 +0200

    Fixed ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'chatroom#index'
   post 'message', to: 'messages#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount ActionCable.server, at:'/cable'
 end
