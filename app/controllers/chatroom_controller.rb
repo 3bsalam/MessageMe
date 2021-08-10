ccbb376c6bc24f8f0fb92a6d8ceacfa1162320b7
Mostafa abdelsalam <mostafa.abdelsalam96@gmail.com>
2021-08-10 15:03:23 +0200

    Add messages from UI

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,7 +1,9 @@
 class ChatroomController < ApplicationController
-
+    before_action :authenticate_user!
 
     def index
+        @message = Message.new
+        @messages = Message.all
     end
 
     
