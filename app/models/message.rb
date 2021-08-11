d5afa762a22b72b0acb81257cda6d236b0d9c2fb
Mostafa abdelsalam <mostafa.abdelsalam96@gmail.com>
2021-08-11 10:56:47 +0200

    Added sending message using enter button and limited displaying messages to last 20

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: {maximum: 50}
   
+  scope :custom_display, -> { order(:created_at).last(20)}
   
   end
   
